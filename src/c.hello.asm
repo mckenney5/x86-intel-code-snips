b14a62750c52520878a7200613cfca510c197c12
mckenney5 <mckenneyadam@gmail.com>
2018-08-24 19:23:28 -0400

    added hello worlds
    - c.hello.asm: uses 'puts' from glibc to display text
    - linux.hello.asm: uses sys_call write to display text via the kernel

---
+++ src/c.hello.asm
@@ -0,0 +1,24 @@
+; Writes 'hello, world' to stdout via puts (glibc)
+; x86 32bit only
+
+SECTION .data			; initalized data
+	msg: db "Hello, World",0 ; the mesg to display
+
+SECTION .text			; the code
+	global main		; main used so gcc can be linker
+				; can be changed to start for ld
+	extern puts
+
+main:
+	push ebp		; prolog
+	mov ebp, esp
+
+	push msg		; push the msg to the stack
+	call puts		; calls puts, puts get msg from stack
+	;pop msg			; removes msg from stack
+	
+	mov esp, ebp		; epilog
+	pop ebp
+	
+	ret
+
---
+++ src/linux.hello.asm
@@ -0,0 +1,48 @@
+; Writes hello, world to stdout via linux sys_calls
+; x86 32bit Linux kernel only
+
+SECTION .data			; initalized data
+	msg: db "Hello, World",10,0 	; the mesg to display
+	msg_len equ $ - msg 	; msg's length
+
+SECTION .text			; the code
+	global main		; main used so gcc can be linker
+				; can be changed to start for ld
+
+main:
+	push ebp		; prolog
+	mov ebp, esp
+
+	mov ecx, msg		; display msg
+	mov edx, msg_len	; with msg's length
+	call sys_write		; sys_call write(msg, len)
+	
+	call done		; clean up and exit
+
+sys_write:
+	; write(msg, len)
+	;       ecx, edx
+
+	push ebp		; prolog
+	mov ebp, esp
+	
+	mov eax, 4		; sys_call write
+	mov ebx, 1		; stdout
+	int 0x80		; call kernel
+	
+	mov esp, ebp		; epilog
+	pop ebp
+	ret
+
+done:
+	; Revert stack pointer to the base
+	; Then tell the kernel that we are done
+
+	mov esp, ebp		; epilog
+	pop ebp
+
+	mov eax, 1		; sys_call exit
+	mov ebx, 0		; exit(0);
+	int 0x80		; call the kernel
+	ret
+
