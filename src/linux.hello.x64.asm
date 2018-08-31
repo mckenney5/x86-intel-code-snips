d92d6fcfea4f00eee533cade22d85e1783484643
mckenney5 <mckenneyadam@gmail.com>
2018-08-31 16:05:07 -0400

    added a x86_64 example of hello world

---
+++ src/linux.hello.x64.asm
@@ -0,0 +1,20 @@
+; Hello World using system calls
+; for x86_64 Linux only
+
+section .data				; constants
+	text db "Hello, World",10	; const char* Hello World
+
+section .text				;function declaring
+	global _start
+
+_start:					; main()
+	mov rax, 1			; sys_write
+	mov rdi, 1			; write to STDOUT
+	mov rsi, text			; pointer to text
+	mov rdx, 14			; text length
+	syscall				; sys_write(STDOUT, text&, size_of(text));
+	
+	mov rax, 60			; call sys_exit
+	mov rdi, 0			; exit with error code 0
+	syscall				; sys_exit(0)
+
